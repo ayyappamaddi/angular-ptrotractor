af105115b7e5c2345c1b5385a575abc18f796032
ayyappamaddi <ayyappa.maddi@valuelabs.com>
2020-03-10 00:02:01 +0530

    lets try with headless

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -11,7 +11,7 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome',
     'chromeOptions': {
-      args: ['--disable-browser-side-navigation']
+      args: ['--disable-browser-side-navigation','--headless']
     }
     // 'browserName': 'firefox',
     // 'moz:firefoxOptions': {
