a85fc3440a0578c4cad7528aa0e7aa3bfd5fc05e
ayyappamaddi <ayyappa.maddi@valuelabs.com>
2020-03-10 00:27:14 +0530

    trying with global installation

--- scripts/frontend-dependencies.sh
+++ scripts/frontend-dependencies.sh
@@ -1,5 +1,7 @@
 echo "Frontedn-dependencies.sh"
 npm install
 echo 'node modules installed'
+npm install -g protractor
+echo 'install protractor globally'
 npm run e2e
 echo 'e2e test cases executed.'
