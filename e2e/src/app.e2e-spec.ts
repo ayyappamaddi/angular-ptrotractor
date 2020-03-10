5670f2feeb62dbd3684f2a9c8ce90787caa88c61
ayyappamaddi <ayyappa.maddi@valuelabs.com>
2020-03-10 21:54:47 +0530

    trying with google.com

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -14,21 +14,29 @@ describe('workspace-project App', () => {
   });
 
   it('should display welcome message', () => {
-    browser.waitForAngularEnabled(false)
-    browser.get('https://acquire.qa.altusplatform.com');
-    // page.navigateTo();
+    browser.waitForAngularEnabled(false);
+    browser.get('https://google.com');
     var EC = protractor.ExpectedConditions;
-     browser.wait(EC.presenceOf(element(by.id('sfid-username'))), 30000, 'Element taking too long to appear in the DOM');
+    browser.wait(EC.presenceOf(element(by.id('main'))), 3000, 'Element taking too long to appear in the DOM');
+    expect(1).toEqual(1);
 
-    element(by.id('sfid-username')).sendKeys('santosh+admin180919124500@argusexpresstest.com');
-    element(by.id('sfid-password')).sendKeys('Password123$');
-    var getStarted = element(by.id('sfid-submit'));
-    getStarted.click();
-      browser.wait(EC.presenceOf(element(by.className('apex__header-container'))), 60000, 'Element taking too long to appear in the DOM');
 
-    // browser.waitForAngularEnabled(true);
-    const appText = getText('.apex__header-container span');
-    console.log('appText==>', appText)
-    expect(appText).toEqual('ARGUS ACQUIRE');
+
+    // browser.waitForAngularEnabled(false)
+    // browser.get('https://acquire.qa.altusplatform.com');
+    // // page.navigateTo();
+    // var EC = protractor.ExpectedConditions;
+    //  browser.wait(EC.presenceOf(element(by.id('sfid-username'))), 30000, 'Element taking too long to appear in the DOM');
+
+    // element(by.id('sfid-username')).sendKeys('santosh+admin180919124500@argusexpresstest.com');
+    // element(by.id('sfid-password')).sendKeys('Password123$');
+    // var getStarted = element(by.id('sfid-submit'));
+    // getStarted.click();
+    //   browser.wait(EC.presenceOf(element(by.className('apex__header-container'))), 60000, 'Element taking too long to appear in the DOM');
+
+    // // browser.waitForAngularEnabled(true);
+    // const appText = getText('.apex__header-container span');
+    // console.log('appText==>', appText)
+    // expect(appText).toEqual('ARGUS ACQUIRE');
   });
 });
