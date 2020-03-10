447c9582005a40068d717c190f2e1d5fcc377fa2
ayyappamaddi <ayyappa.maddi@valuelabs.com>
2020-03-10 18:55:21 +0530

    web driver start

--- scripts/frontend-dependencies.sh
+++ scripts/frontend-dependencies.sh
@@ -3,5 +3,7 @@ npm install
 echo 'node modules installed'
 npm install -g protractor
 echo 'install protractor globally'
+webdriver-manager update --standalone  --gecko false
+webdriver-manager start
 npm run e2e
 echo 'e2e test cases executed.'
