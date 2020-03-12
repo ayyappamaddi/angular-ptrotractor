190b32a956ffe1115f89c0838eace052359bf047
ayyappamaddi <ayyappa.maddi@valuelabs.com>
2020-03-12 13:04:20 +0530

    a

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -11,7 +11,7 @@ exports.config = {
   capabilities: {
     'browserName': 'chrome',
     'chromeOptions': {
-      args: ['--disable-browser-side-navigation','--headless']
+      args: ['headless']
     }
     // 'browserName': 'firefox',
     // 'moz:firefoxOptions': {
