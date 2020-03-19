4d8cc263040d074333aeecbbff46ccd8adf77938
ayyappamaddi <ayyappa.maddi@valuelabs.com>
2020-03-19 12:28:04 +0530

    detach webriver manager

--- scripts/frontend-dependencies.sh
+++ scripts/frontend-dependencies.sh
@@ -6,6 +6,6 @@ npm install protractor -g
 echo 'installed protractor globally'
 webdriver-manager shutdown
 webdriver-manager update --standalone  --gecko false
-webdriver-manager start
+webdriver-manager start --detach
 npm run e2e
 echo 'e2e test cases executed.'
